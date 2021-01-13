1610b26b7ea70f8f019e3cf96513b589b0fcdacb
Giorgia Borgmann <66149711+GiorgiaBorgmann@users.noreply.github.com>
2021-01-13 20:36:32 +0200

    auth0 register and getting user information working

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const { auth, requiresAuth } = require('express-openid-connect');
 const router = express.Router();
 
 const { user } = require('../controllers/user');
+const { register } = require('../controllers/auth');
+const { profile } = require('../controllers/auth');
 
 // user routes
 router.get('/user/:id', user);
+router.get('/', register)
+router.get('/profile', requiresAuth, profile)
+
+module.exports = router
