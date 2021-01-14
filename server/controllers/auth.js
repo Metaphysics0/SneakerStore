67e4d6c104f7810918135254d0eb684166a11c94
Giorgia Borgmann <66149711+GiorgiaBorgmann@users.noreply.github.com>
2021-01-14 15:37:46 +0200

    Auth0 configurated

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -3,7 +3,8 @@ const { auth, requiresAuth } = require('express-openid-connect');
 
 // req.isAuthenticated is provided from the auth router
 exports.register = (req, res) => {
-    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out')
+
+    res.send(req.oidc.user)
 };
 
 
