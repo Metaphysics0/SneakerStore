e8e2a49e83096addc1fde463a4265730882aa8fb
NEIL1821 <69971394+NEIL1821@users.noreply.github.com>
2021-01-14 15:48:04 +0200

    Changed port to 3000

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ app.use(bodyParser.json());
 app.use('/api', routes);
 
 // server
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
