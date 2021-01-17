1a4dae91c6d263930a3421755ba1337cf7e8230d
NEIL1821 <69971394+NEIL1821@users.noreply.github.com>
2021-01-17 13:39:38 +0200

    image-handling sytax errors fixed

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,11 +1,10 @@
 
-const { auth, requiresAuth } = require('express-openid-connect');
+// const { auth, requiresAuth } = require('express-openid-connect');
 
 // req.isAuthenticated is provided from the auth router
 exports.register = (req, res) => {
     res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out')
 };
-
 
 exports.profile = (req, res) => {
     res.send(JSON.stringify(req.oidc.user));
