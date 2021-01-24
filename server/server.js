f3c191ebe98d601368bb51a8b6f839e6182ca530
NEIL1821 <69971394+NEIL1821@users.noreply.github.com>
2021-01-24 14:04:35 +0200

    not even sure what changes here its referring to, ignore

--- server/server.js
+++ server/server.js
@@ -50,7 +50,7 @@ app.use(bodyParser.json());
 app.use('/api', routes);
 
 // server
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
