4ac56be553ee7e66df1d74f9518e5cf6fa2dd9ab
Ryan Roberts <ryan@Ryans-MacBook-Pro.local>
2021-01-25 22:59:22 +0200

    sneaker page layout complete

--- client/src/Pages/SneakerPage.jsx
+++ client/src/Pages/SneakerPage.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Data from './data.json';
+import PageBottom from './SneakerPage/PageBottom';
+import PageTop from './SneakerPage/PageTop';
 
 const SneakerPage = () => {
   return (
-    <div>
-      <h1>Sneaker Page!!!</h1>
+    <div className="product">
+      <PageTop Data={Data} />
+      <PageBottom />
     </div>
   );
 };
