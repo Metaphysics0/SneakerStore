47d726c6b31c8f5da7d70f571f0e58134525705e
Ryan Roberts <ryan@Ryans-MacBook-Pro.local>
2021-01-25 15:23:05 +0200

    dynamic search

--- client/src/Pages/Search.jsx
+++ client/src/Pages/Search.jsx
@@ -1,121 +1,52 @@
-import React from 'react';
-import { shoe } from './mockData';
-import { AiFillStar } from 'react-icons/ai';
-import { FaRegHeart } from 'react-icons/fa';
+import React, { useState } from 'react';
 import Logo from '../img/logos/another-1.png';
-import { Link } from 'react-router-dom';
+import Data from './data.json';
+import Data2 from './data2.json';
+import ShoeCard from '../Components/ShoeCard';
 
 const Search = () => {
-  const star = () => <AiFillStar className="gold" />;
-  const rating = (num) => [...Array(num)].map(star);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  // Dynamic Search
+  const results = !searchTerm
+    ? Data.shoes
+    : Data.shoes.filter(
+        (shoe) =>
+          shoe.brand.toLowerCase().includes(searchTerm) ||
+          shoe.name.toLowerCase().includes(searchTerm) ||
+          shoe.brand.includes(searchTerm) ||
+          shoe.name.includes(searchTerm)
+      );
+
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
 
   return (
     <>
       <div className="search">
         <img className="search__logo mb-3" src={Logo} alt="logo" />
-        <input className="search__input mb-2" type="text" name="search" placeholder="Search..." />
-      </div>
-
-      <div className="heading__impact mb-3 clamp-1">Featured Products</div>
-      <div className="shoe">
-        <div className="shoe__content">
-          <img className="shoe__img mb-2" src={shoe.img} alt="shoe" />
-          <div className="bg-grey">
-            <p className="shoe__title mb-1">
-              {shoe.brand} {shoe.name} <br /> ({shoe.year})
-            </p>
-            <p className="shoe__price">
-              Price: &nbsp;<span>${shoe.price}</span>
-            </p>
-            <p className="shoe__sellar">
-              Seller: {shoe.sellar}&nbsp;{rating(shoe.sellarRating)}
-            </p>
-            <p className="shoe__views mb-1">Views: {shoe.views}</p>
-            <p className="shoe__date">Date Listed: {shoe.dateListed}</p>
-            <div className="space-between clamp-1">
-              <Link to="/product/123">
-                <button className="shoe__btn">View</button>
-              </Link>
-              <FaRegHeart className="shoe__icon" />
-            </div>
-          </div>
-        </div>
-
-        <div className="shoe__content">
-          <img
-            className="shoe__img mb-2"
-            src={shoe.img}
-            alt="shoe"
-            onLoad={() => <h3>Loading!!</h3>}
-          />
-          <div className="bg-grey">
-            <p className="shoe__title mb-1">
-              {shoe.brand} {shoe.name} <br /> ({shoe.year})
-            </p>
-            <p className="shoe__price">
-              Price: &nbsp;<span>${shoe.price}</span>
-            </p>
-            <p className="shoe__sellar">
-              Seller: {shoe.sellar}&nbsp;{rating(shoe.sellarRating)}
-            </p>
-            <p className="shoe__views mb-1">Views: {shoe.views}</p>
-            <p className="shoe__date">Date Listed: {shoe.dateListed}</p>
-            <div className="space-between clamp-1">
-              <Link to="/product/123">
-                <button className="shoe__btn">View</button>
-              </Link>
-              <FaRegHeart className="shoe__icon" />
-            </div>
-          </div>
-        </div>
-
-        <div className="shoe__content">
-          <img className="shoe__img mb-2" src={shoe.img} alt="shoe" />
-          <div className="bg-grey">
-            <p className="shoe__title mb-1">
-              {shoe.brand} {shoe.name} <br /> ({shoe.year})
-            </p>
-            <p className="shoe__price">
-              Price: &nbsp;<span>${shoe.price}</span>
-            </p>
-            <p className="shoe__sellar">
-              Seller: {shoe.sellar}&nbsp;{rating(shoe.sellarRating)}
-            </p>
-            <p className="shoe__views mb-1">Views: {shoe.views}</p>
-            <p className="shoe__date">Date Listed: {shoe.dateListed}</p>
-            <div className="space-between clamp-1">
-              <Link to="/product/123">
-                <button className="shoe__btn">View</button>
-              </Link>
-              <FaRegHeart className="shoe__icon" />
-            </div>
-          </div>
-        </div>
-
-        <div className="shoe__content">
-          <img className="shoe__img mb-2" src={shoe.img} alt="shoe" />
-          <div className="bg-grey">
-            <p className="shoe__title mb-1">
-              {shoe.brand} {shoe.name} <br /> ({shoe.year})
-            </p>
-            <p className="shoe__price">
-              Price: &nbsp;<span>${shoe.price}</span>
-            </p>
-            <p className="shoe__sellar">
-              Seller: {shoe.sellar}&nbsp;{rating(shoe.sellarRating)}
-            </p>
-            <p className="shoe__views mb-1">Views: {shoe.views}</p>
-            <p className="shoe__date">Date Listed: {shoe.dateListed}</p>
-            <div className="space-between clamp-1">
-              <Link to="/product/123">
-                <button className="shoe__btn">View</button>
-              </Link>
-              <FaRegHeart className="shoe__icon" />
-            </div>
-          </div>
-        </div>
+        <input
+          className="search__input mb-2"
+          type="text"
+          name="search"
+          placeholder="Search..."
+          value={searchTerm}
+          onChange={handleChange}
+        />
       </div>
       <h3 className="heading__impact mb-3 clamp-1">Recent listings</h3>
+      <div className="shoe mb-4">
+        {results.map((shoe) => (
+          <ShoeCard key={shoe.id} shoe={shoe} />
+        ))}
+      </div>
+      <div className="heading__impact mb-3 clamp-1">Featured Products</div>
+      <div className="shoe">
+        {Data2.shoes.map((shoe) => (
+          <ShoeCard shoe={shoe} />
+        ))}
+      </div>
     </>
   );
 };
