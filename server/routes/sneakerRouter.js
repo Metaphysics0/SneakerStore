f950a31765f1dd28befc356a597986c70506df49
Ryan Roberts <ryan@Ryans-MacBook-Pro.local>
2021-01-26 23:35:55 +0200

    mongo integration-be

--- server/routes/sneakerRouter.js
+++ server/routes/sneakerRouter.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const { addSneaker, getSneaker } = require('../controllers/sneakerCtrl');
+const { addSneaker, getSneaker, getAllSneakers } = require('../controllers/sneakerCtrl');
 
 //maybe to clear stuff up we will make a new js file for sneakerRouter
 router.post('/addSneakerDB', addSneaker);
 
-router.get('/getSneakerById/:id', getSneaker)
+router.get('/get-sneaker/:id', getSneaker);
+
+router.get('/get-sneakers', getAllSneakers);
 
 module.exports = router;
